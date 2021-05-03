c35bdf255521bb52dc76f5821414b60e917cf1b7
weekend37 <helgi37@gmail.com>
2021-05-03 12:24:08 -0700

    multithreading across samples, not k-mers

--- Utils/string_kernel.py
+++ Utils/string_kernel.py
@@ -4,38 +4,60 @@ from functools import partial
 
 slide_window = np.lib.stride_tricks.sliding_window_view
 
-def sum_over_mz(m,z):
-    return np.sum(np.all( np.lib.stride_tricks.sliding_window_view(z,m,axis=2), axis=3),axis=2)
+def sum_over_mZ(m,Z):
+    return np.sum(np.all( np.lib.stride_tricks.sliding_window_view(Z,m,axis=2), axis=3),axis=2)
+
+def sum_over_zM(z, Ms):
+    return np.sum([np.sum(np.all(np.lib.stride_tricks.sliding_window_view(z,m,axis=1), axis=2),axis=1) for m in Ms], axis=0)
+
+def string_kernel(X, Y, n_jobs=None, m_axis="samples"):
+    
+    Z = np.array([np.equal(X_i, Y) for X_i in X])
+    Ms = range(1,Z.shape[-1])
+    
+    if n_jobs == 1 or m_axis is "None":
+        K = np.sum( np.array( [sum_over_mZ(m,Z) for m in Ms] ), axis=0 )
+
+    elif m_axis == "k-mers":
+        with mp.Pool(n_jobs) as pool:
+            sums = pool.map(partial(sum_over_mZ, Z=Z), Ms)
+        K = np.sum(sums,axis=0)
+
+    elif m_axis == "samples":
+        with mp.Pool(n_jobs) as pool:
+            K = np.array(pool.map(partial(sum_over_zM, Ms=Ms),  Z))
+
+    return K
+
+def random_string_kernel(X, Y, alpha=1, n_jobs=None, m_axis="samples", seed=37):
+    
+    np.random.seed(seed)
+    Z = np.array([np.equal(X_i, Y) for X_i in X])
+    Ms = [m for m in range(1,Z.shape[-1]) if np.random.rand() <= (1/m**alpha)]
+    
+    if n_jobs == 1 or m_axis is "None":
+        K = np.sum( np.array( [sum_over_mZ(m,Z) for m in Ms] ), axis=0 )
+
+    elif m_axis == "k-mers":
+        with mp.Pool(n_jobs) as pool:
+            sums = pool.map(partial(sum_over_mZ, Z=Z), Ms)
+        K = np.sum(sums,axis=0)
+
+    elif m_axis == "samples":
+        with mp.Pool(n_jobs) as pool:
+            K = np.array(pool.map(partial(sum_over_zM, Ms=Ms),  Z))
+
+    return K
 
 def string_kernel_singlethread(X, Y):
     Z = np.array([np.equal(X_i, Y) for X_i in X])
-    return np.sum( np.array( [m*sum_over_mz(m,Z) for m in range(Z.shape[-1])] ), axis=0 )
-
-def string_kernel(X, Y, n_jobs=None):
-
-    Z = np.array([np.equal(X_i, Y) for X_i in X])
-        
-    with mp.Pool(n_jobs) as pool:
-        sums = pool.map(partial(sum_over_mz, z=Z), range(Z.shape[-1]))
-
-    return np.sum(sums,axis=0)
-
+    return np.sum( np.array( [m*sum_over_mZ(m,Z) for m in range(Z.shape[-1])] ), axis=0 )
+    
 def random_string_kernel_singlethread(X, Y, alpha=1, seed=37):
     np.random.seed(seed)
     Z = np.array([np.equal(X_i, Y) for X_i in X])
     Ms = [m for m in range(1,Z.shape[-1]) if np.random.rand() < (1/m**alpha)]
-    return np.sum( np.array( [m*sum_over_mz(m,Z) for m in Ms] ), axis=0 )
-
-def random_string_kernel(X, Y, alpha=1, n_jobs=None, seed=37):
-    
-    np.random.seed(seed)
-    Z = np.array([np.equal(X_i, Y) for X_i in X])
-    Ms = [m for m in range(1,Z.shape[-1]) if np.random.rand() < (1/m**alpha)]
-    
-    with mp.Pool(n_jobs) as pool:
-        sums = pool.map(partial(sum_over_mz, z=Z), Ms)
-
-    return np.sum(sums,axis=0)
+    return np.sum( np.array( [sum_over_mZ(m,Z) for m in Ms] ), axis=0 )
 
 ## ------------------------------- other kernels ------------------------------- ##
 
@@ -44,21 +66,6 @@ def linear_kernel(X,Y):
 
 def hamming_kernel(X, Y):
     return np.dot(X,Y.T) + np.dot((1-X), (1-Y).T)
-
-def string_kernel_singly_vectorized(X, Y):
-    # From early stages of development - could be useful for 
-    # different types of parallelizations
-    nx, mx = X.shape
-    ny, my = Y.shape
-    M = mx
-
-    K = np.zeros( (nx,ny) )
-    for i, X_i in enumerate(X):
-        Z_i = np.equal(X_i, Y).astype(int)
-        for m in range(M):
-            K[i,:] += np.sum(np.all(slide_window(Z_i,m,axis=1), axis=2),axis=1)
-
-    return K
 
 def substring_kernel_vectorized(X, Y, M=5, stride=1):
         
